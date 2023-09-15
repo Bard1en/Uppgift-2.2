6551baa21c7dafc049f7cda4b385d4e8572454d1
losc06001 <losc06001@E-PW0234JE>
2023-09-15 12:38:10 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ namespace Uppgift2._2 
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma");
+            string y = Console.ReadLine();
+            double z = double.Parse(x);
+            double g = double.Parse(y);
+            double sum = z - g;
+            Console.WriteLine("Elin hoppade " +sum +  " Längre");
+
+        }
+    }
+}
